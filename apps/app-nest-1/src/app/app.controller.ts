8db26cc39afcce2b595f03939b036efc2f0e22cb
Pavel Balashov <pavel.balashov1@gmail.com>
2024-09-12 10:59:43 +0200

    feat: implement request endpoint

--- apps/app-nest-1/src/app/app.controller.ts
+++ apps/app-nest-1/src/app/app.controller.ts
@@ -11,7 +11,10 @@ import { diskStorage } from 'multer';
 import { extname, join } from 'path';
 import { v4 as uuidv4 } from 'uuid';
 import type { Express } from 'express';
-import { createReadStream } from 'fs';
+import { createReadStream, createWriteStream } from 'fs';
+import { Readable } from 'stream';
+import { ReadableStream as NodeReadableStream } from 'stream/web';
+import { pipeline } from 'stream/promises';
 
 @Controller()
 export class AppController {
@@ -47,5 +50,33 @@ export class AppController {
       type: 'application/pdf',
       disposition: 'attachment; filename="google.pdf"',
     });
+  }
+
+  @Post('request')
+  async downloadFileToUploads() {
+    const url = 'http://localhost:3000/api/download';
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to download file: ${response.statusText}`);
+    }
+
+    const filename = `${uuidv4()}.pdf`;
+    const fileStream = createWriteStream(
+      join(process.cwd(), 'uploads', filename),
+    );
+
+    const nodeStream = Readable.fromWeb(
+      response.body as unknown as NodeReadableStream,
+    );
+
+    try {
+      await pipeline(nodeStream, fileStream);
+      return {
+        message: `File downloaded and saved successfully as ${filename}`,
+      };
+    } catch (error) {
+      console.error('Failed to download and save file:', error);
+      throw new Error('File download failed');
+    }
   }
 }
