9f95985314b6b959735a47a8fe8e97199243ae3c
Pavel Balashov <pavel.balashov1@gmail.com>
2024-09-12 10:20:28 +0200

    feat: implement upload endpoint

--- apps/app-nest-1/src/app/app.controller.ts
+++ apps/app-nest-1/src/app/app.controller.ts
@@ -1,13 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
-
-import { AppService } from './app.service';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname, join } from 'path';
+import { v4 as uuidv4 } from 'uuid';
+import type { Express } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: join(process.cwd(), 'uploads'),
+        filename: (req, file, cb) => {
+          const filename = `${uuidv4()}${extname(file.originalname)}`;
+          cb(null, filename);
+        },
+      }),
+      fileFilter: (req, file, cb) => {
+        if (!file.originalname.match(/\.pdf$/)) {
+          cb(new Error('Only PDF files are allowed'), false);
+          return;
+        }
+        cb(null, true);
+      },
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return { message: 'File uploaded successfully!', file: file.filename };
   }
 }
--- apps/app-nest-1/src/app/app.module.ts
+++ apps/app-nest-1/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
@@ -14,6 +12,5 @@ import { ConfigModule } from '@nestjs/config';
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
